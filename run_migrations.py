caa832c32118325394efab5cff0621850567e409
umar <umarsa8080@gmail.com>
2026-06-25 18:24:05 +0100

    Add auto-migration script to Render build process

---
+++ run_migrations.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+import os
+import django
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "yajus_kitchen.settings")
+django.setup()
+
+from django.core.management import call_command
+
+# First, fake the kitchen app migrations since those tables already exist
+try:
+    call_command('migrate', '--fake', 'kitchen')
+except Exception as e:
+    print(f"Note (kitchen migrations already applied: {e}")
+
+# Now run all other migrations normally
+call_command('migrate')
